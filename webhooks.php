501c9ff0c02197b8bedbc0ec8ad9ff103a3b39f3
rangsimanch <12jniw.t@gmail.com>
2019-05-15 13:20:27 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'lX/e3Bly95uiDmgSwynTy80ssri9AYkkRUrtdjTOEDfQJ/kC5L2e/bT9/lEXVTBGbaoNnMVpuDswiBaNgDr+hCcr0FmBgl9JnDMbYi5pGvTBWKuN3avvLzoLDX7QvwNamZdmxrzZ4O/SL/JiG99XxAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
