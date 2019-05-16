88b86807738333649db84e0ccb52ee5ed8d94a07
rangsimanch <12jniw.t@gmail.com>
2019-05-16 11:44:48 +0700

    Update pg_connect.php

--- pg_connect.php
+++ pg_connect.php
@@ -22,7 +22,54 @@ if (!$dbconn4) {
     exit;
   }
 
-  $result = pg_query($dbconn4,"SELECT * FROM "public".pos_config");
+  $query_str = "SELECT
+  CAST("public".pos_session.stop_at AS DATE),
+  "public".pos_config."name",
+  Sum(ceiling(("public".pos_order_line.qty * "public".pos_order_line.price_unit) - floor("public".pos_order_line.qty * "public".pos_order_line.price_unit * ("public".pos_order_line.discount / 100::numeric)))) AS grantotalsum,
+  CASE WHEN "public".pos_config."name" LIKE '%วัดบูรพ์%' OR 
+                      "public".pos_config."name" LIKE '%เดอะมอล%' OR
+                      "public".pos_config."name" LIKE '%เซฟวัน%' OR
+                      "public".pos_config."name" LIKE '%มหาราช%' OR
+                      "public".pos_config."name" LIKE '%ราชสีมา%' OR
+                      "public".pos_config."name" LIKE '%คลัง%' OR
+                      "public".pos_config."name" LIKE '%มทส%' OR
+                      "public".pos_config."name" LIKE '%จอม%' OR
+                      "public".pos_config."name" LIKE '%เซ็น%' 
+              THEN 'NMA'
+              
+              WHEN "public".pos_config."name" LIKE '%ขอนแก่น%'
+              THEN 'KKN'
+              
+              WHEN "public".pos_config."name" LIKE '%บุรี%'
+              THEN 'BRM'
+              
+              WHEN "public".pos_config."name" LIKE '%อุดร%'
+              THEN 'UDN'
+              
+              WHEN "public".pos_config."name" LIKE '%MSIG%' OR
+              "public".pos_config."name" LIKE '%XT%' OR
+              "public".pos_config."name" LIKE '%Siam%' OR
+              "public".pos_config."name" LIKE '%East%'
+              THEN 'BKK'
+              
+           ELSE 'null' END AS name_province
+  FROM
+  "public".pos_session
+  INNER JOIN "public".pos_order ON "public".pos_order.session_id = "public".pos_session."id"
+  INNER JOIN "public".pos_order_line ON "public".pos_order_line.order_id = "public".pos_order."id"
+  INNER JOIN "public".pos_config ON "public".pos_session.config_id = "public".pos_config."id"
+  WHERE
+  CAST("public".pos_session.stop_at AS DATE) >= '2019-01-01'::date
+  GROUP BY
+  "public".pos_session."id",
+  CAST("public".pos_session.stop_at AS DATE),
+  "public".pos_config."name"
+  ORDER BY
+  CAST("public".pos_session.stop_at AS DATE) ASC
+  "
+
+
+  $result = pg_query($dbconn4,$query_str);
   if (!$result) {
     echo "An error Query !!!!.\n";
     exit;
