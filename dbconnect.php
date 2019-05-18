3430d787299ddd9d68a46b067100a146263e4dca
rangsimanch <12jniw.t@gmail.com>
2019-05-18 17:46:45 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -2,18 +2,10 @@
 
 $conn_string = "host=96.30.79.11 port=5432 dbname=LIV user=odoo password=odoo";
 
-//connect to a database named "test" on the host "sheep" with a username and password
-
-$str_output ="";
-
 function OpenCon()
  {
- $dbhost = "96.30.79.11";
- $port = "5432";
- $dbuser = "odoo";
- $dbpass = "odoo";
- $db = "LIV";
- $conn = pg_connect($dbhost. " " .$port. " " .$db. " ".$dbuser. " " .$dbpass) or die 'Connect Failed';
+
+ $conn = pg_connect($conn_string) or die 'Connect Failed';
  
  return $conn;
  }
