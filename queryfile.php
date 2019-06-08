7b431055e5699e0eb25b4d66162872656af9bc84
rangsimanch <12jniw.t@gmail.com>
2019-06-08 15:32:36 +0700

    Update queryfile.php

--- queryfile.php
+++ queryfile.php
@@ -26,11 +26,11 @@
     foreach ($json_array as $key => $value) {
       if($value[0] != ""){
         if($i == 0){
-          $str_result = $str_result . "วันที่: " . $value["0"] . '\n' ."สาขา: " . $value["1"] . " ยอดขาย: " . $value["2"] . ".-" . '\n';
+          $str_result = $str_result . "วันที่: " . $value["0"]  ."สาขา: " . $value["1"] . " ยอดขาย: " . $value["2"] . ".-";
           $i++;
         }
         else{
-          $str_result = $str_result . " สาขา: " . $value["1"] . " ยอดขาย: " . $value["2"] . ".-". '\n';
+          $str_result = $str_result . " สาขา: " . $value["1"] . " ยอดขาย: " . $value["2"] . ".-";
         }
       }
     } 
